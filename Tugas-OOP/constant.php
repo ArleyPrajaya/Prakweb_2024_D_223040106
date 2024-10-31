229cabf7074d296783a4bf90659fee9145aeac78
ArleyPrajaya <vior497@gmail.com>
2024-11-01 04:08:59 +0700

    Tugas Video OOP -12(Constant)

---
+++ Tugas-OOP/constant.php
@@ -0,0 +1,16 @@
+<?php 
+// define ('NAMA', 'Arley Prajaya G');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 21;
+// echo UMUR;
+
+class Coba {
+    const NAMA = 'Arley Prajaya Gunawan';
+}
+
+echo Coba::NAMA;
+
+?>
